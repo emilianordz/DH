1cf3de65c76dc0e7d5cf25164ae3dd06a6dace15
Emilianordz <rodriguezeme93@gmail.com>
2023-01-02 22:51:13 -0300

    configuracion port

--- Mercado Liebre/app.js
+++ Mercado Liebre/app.js
@@ -5,9 +5,9 @@ const app= express();
 
 app.use(express.static('public'));
 
-
-app.listen(3030, () => {
-    console.log('Servidor iniciado en http://localhost:3030');
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+    console.log('Servidor iniciado en http://localhost:' + port);
 });
 
 app.get('/', (req, res) =>{
